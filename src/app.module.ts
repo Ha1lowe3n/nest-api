b9257e6767ed9fb7f85be9e196bda1f352fedce6
Ha1lowe3n <Ha1lowe3n@yandex.ru>
2021-10-28 07:53:22 +0800

    clear project, start again learn nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TagsModule } from './tags/tags.module';
-import { UserModule } from './user/user.module';
-import config from './ormconfig';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(config), TagsModule, UserModule],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/types/userResponse.interface.ts
+++
@@ -1,5 +0,0 @@
-import { UserEntity } from '../user/user.entity';
-
-export interface UserResponseInterface {
-    user: Omit<UserEntity, 'hashPassword'> & { token: string };
-}
