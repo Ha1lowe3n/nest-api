c233d163ff458fc8273179f96dd1cd2735774411
Ha1lowe3n <Ha1lowe3n@yandex.ru>
2021-10-28 11:27:32 +0800

    add typeorm config, connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagsModule } from './tags/tags.module';
 
 @Module({
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+        }),
+        TagsModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
-    imports: [TagsModule],
 })
 export class AppModule {}
